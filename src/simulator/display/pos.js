a963d32e1063a52f3516853ccbd26afc3db3b94d
Bret Lorimore <bretlorimore@gmail.com>
2016-12-02 13:03:35 -0800

    work on simulator view and simulator general structure

--- src/simulator/display/pos.js
+++ src/simulator/display/pos.js
@@ -1,63 +1,93 @@
-function initSim() {
-    var view = new View();
-    view.init();
-}
+// EclipseSimulator namespace
+var EclipseSimulator = {
 
-// Jake, this fixes the issue you were seeing where when width was set to 100%
-// you were getting a 0 value - this is because the code was executing before that
-// svg element was actually rendered/had a width associated with it. This jQuery function
-// below, $(document).ready takes a function as an argument and calls it when the document
-// is ready, i.e. when that svg has a width associated with it.
-//
-// Also, it is better practice call your init methods from within your js file than from
-// within your HTML, that is why I commented out this call in the HTML file.
-$(document).ready(initSim);
+    View: function()
+    {
+        this.window   = $('#container').get(0);
+        this.controls = $('#controls').get(0);
+        this.sun      = $('#sun').get(0);
+        this.moon     = $('#moon').get(0);
+
+        this.sunpos  = {x: 50, y: 50};
+        this.moonpos = {x: 25, y: 25};
+    },
+
+    Controller: function()
+    {
+        this.view  = new EclipseSimulator.View();
+        // this.model = new EclipseSimulator.Model();
+    },
+
+    // Model: function() {},
+};
 
 
-// I think now is a good time to start working the view as a "class". This is because the
-// view should have some properties associated with it, like the sun/moon objects. Also
-// this results in ultimately cleaner code. JavaScript "classes" are actually functions.
-// Here we define the View "class". Instances of it can be instantiated like this:
 //
-//  var view = new View();
+// EclipseSimulator.View methods
 //
-function View()
+EclipseSimulator.View.prototype.init = function()
 {
-    // $('#cont') is jQuery shorthand for document.getElementById("cont")
-    // but returns an array - that is why we call get(0) on it, this is the same
-    // as $('#cont')[0]
-    this.window = $('#cont').get(0);
-    this.sun    = $('#sun').get(0);
-    this.moon   = $('#moon').get(0);
-}
+    this.refresh();
 
-// These prototype functions act as methods of the class, i.e. they can be called like this:
-//
-//  var view = new View();
-//  view.init();
-//
-View.prototype.init = function()
-{
-    this.position_body_at_percent_coords(this.sun, 25, 50);
-    this.position_body_at_percent_coords(this.moon, 50, 50);
-}
+    // Have to create a reference to this, because inside the window
+    // refresh function callback this refers to the window object
+    var view = this;
 
-View.prototype.get_window_size = function()
+    // Rescale the window when the parent iframe changes size
+    $(window).resize(function() {
+        view.refresh();
+    });
+};
+
+EclipseSimulator.View.prototype.refresh = function()
 {
-    // This is a dictionary/object and can be accessed as follows:
-    //
-    //  var w = view.get_window_size().width
-    //
+    $(this.window).attr('height', $(window).height() - $(this.controls).height());
+    $(this.window).attr('width', $(window).width());
+
+    $(this.window).show();
+
+    // Position sun/moon. Cannot do this until window is displayed
+    this.position_body_at_percent_coords(this.sun, this.sunpos.x, this.sunpos.y);
+    this.position_body_at_percent_coords(this.moon, this.moonpos.x, this.moonpos.y);
+
+    $(this.sun).show();
+    $(this.moon).show();
+};
+
+EclipseSimulator.View.prototype.get_environment_size = function()
+{
     return {
         width:  this.window.width.baseVal.value,
         height: this.window.height.baseVal.value,
     }
-}
+};
 
-View.prototype.position_body_at_percent_coords = function(target, x_percent, y_percent)
+EclipseSimulator.View.prototype.position_body_at_percent_coords = function(target, x_percent, y_percent)
 {
-    var window_size = this.get_window_size();
+    var env_size = this.get_environment_size();
 
-    target.style.cx = window_size.width * (x_percent / 100);
-    target.style.cy = window_size.height * (y_percent / 100);
+    console.log(env_size);
+
+    target.style.cx = env_size.width * (x_percent / 100);
+    target.style.cy = env_size.height * (y_percent / 100);
+};
+
+
+
+//
+// EclipseSimulator.Controller methods
+//
+EclipseSimulator.Controller.prototype.init = function() 
+{
+    this.view.init();
+    // this.model.init();
 }
+
+
+function initSim() {
+    var controller = new EclipseSimulator.Controller();
+    controller.init();
+}
+
+
+$(document).ready(initSim);
