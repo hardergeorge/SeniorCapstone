f3ef045c9831e0349b4598db9c2840c278df3700
harderg <harderg@oregonstate.edu>
2017-01-11 17:00:12 -0800

    add new branch for the addition of a map

--- src/simulator/simulator.js
+++ src/simulator/simulator.js
@@ -637,4 +637,18 @@ function initSim() {
     controller.init();
 }
 
+$(function() {
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644)
+  };
+  var map = new google.maps.Map($("#map-canvas")[0], mapOptions);
+
+  // listen for the window resize event & trigger Google Maps to update too
+  $(window).resize(function() {
+    // (the 'map' here is the result of the created 'var map = ...' above)
+    google.maps.event.trigger(map, "resize");
+  });
+});
+
 $(document).ready(initSim);
